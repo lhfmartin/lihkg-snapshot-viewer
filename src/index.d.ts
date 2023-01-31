3d8d9627511053e8571c31060fa73c7d55ccb1d0
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2023-01-31 23:41:12 +0800

    Add top bar and directory input

---
+++ src/index.d.ts
@@ -0,0 +1,10 @@
+import 'react';
+
+declare module 'react' {
+  interface InputHTMLAttributes<T> extends HTMLAttributes<T> {
+    // extends React's HTMLAttributes
+    directory?: string;
+    webkitdirectory?: string;
+  }
+}
+// Thanks https://stackoverflow.com/a/72787133
