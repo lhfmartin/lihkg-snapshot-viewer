d0028841ffd4e0f172890d973aa3aaa82ab16308
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2025-10-25 15:33:16 -0300

    Add snapshot tests for MessageCard

--- tests/components/MessageCard.spec.tsx
+++ tests/components/MessageCard.spec.tsx
@@ -3,10 +3,32 @@ import { render } from '@testing-library/react';
 import MessageCard from '@/src/components/MessageCard';
 import Message from '@/src/interfaces/message';
 
-it('renders MessageCard', async () => {
+it('renders MessageCard (poster is original poster)', async () => {
   const message: Message = {
     msg: 'This is a test message',
     msg_num: '1',
+    user_gender: 'M',
+    user_nickname: 'TestUser',
+    user: {
+      user_id: '1',
+      level: '10',
+    },
+    reply_time: 1625079600,
+    like_count: '10',
+    dislike_count: '2',
+    status: 1,
+  };
+
+  const { container } = await render(
+    <MessageCard message={message} originalPosterUserId='1' />,
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('renders MessageCard (poster is male)', async () => {
+  const message: Message = {
+    msg: 'This is a test message',
+    msg_num: '2',
     user_gender: 'M',
     user_nickname: 'TestUser',
     user: {
@@ -14,6 +36,50 @@ it('renders MessageCard', async () => {
       level: '10',
     },
     reply_time: 1625079600,
+    like_count: '10',
+    dislike_count: '2',
+    status: 1,
+  };
+
+  const { container } = await render(
+    <MessageCard message={message} originalPosterUserId='1' />,
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('renders MessageCard (poster is female)', async () => {
+  const message: Message = {
+    msg: 'This is a test message',
+    msg_num: '2',
+    user_gender: 'F',
+    user_nickname: 'TestUser',
+    user: {
+      user_id: '2',
+      level: '10',
+    },
+    reply_time: 1625079600,
+    like_count: '10',
+    dislike_count: '2',
+    status: 1,
+  };
+
+  const { container } = await render(
+    <MessageCard message={message} originalPosterUserId='1' />,
+  );
+  expect(container).toMatchSnapshot();
+});
+
+it('renders MessageCard (poster is admin)', async () => {
+  const message: Message = {
+    msg: 'This is a test message',
+    msg_num: '2',
+    user_gender: 'M',
+    user_nickname: 'LIHKG',
+    user: {
+      user_id: '1',
+      level: '999',
+    },
+    reply_time: 1571379600,
     like_count: '10',
     dislike_count: '2',
     status: 1,
