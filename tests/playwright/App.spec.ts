7f26a9fdd6282d4205bf00d3ce2af5a27f2e0eae
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2025-11-07 00:33:24 -0400

    Add tests

--- tests/playwright/App.spec.ts
+++ tests/playwright/App.spec.ts
@@ -20,7 +20,7 @@ test('Visual regression testing on the home page (initial state)', async ({
   await expect(page).toHaveScreenshot();
 });
 
-test.describe('Test the lihkg-snapshot-viewer application using Playwright', () => {
+test.describe('ACT I', () => {
   let page: Page;
 
   test.beforeAll(async ({ browser }) => {
@@ -34,7 +34,7 @@ test.describe('Test the lihkg-snapshot-viewer application using Playwright', () 
     await fileChooser.setFiles(
       path.join(
         process.env.PWD!,
-        'tests/test-assets/thread_3902532_20250415T224211Z',
+        'tests/test-assets/thread_3902532_20250415T224211Z', // The last message in this thread quotes the second last message, and both messages are short enough to be fully displayed on the screen simultaneously
       ),
     );
     await page.locator('div[id="228"]').waitFor();
@@ -50,6 +50,11 @@ test.describe('Test the lihkg-snapshot-viewer application using Playwright', () 
     expect(await page.locator(Selector.Topbar).innerText()).toStrictEqual(
       'Burger King好食過M記',
     );
+  });
+
+  test('Visual regression testing', async () => {
+    test.skip(!isDarwin);
+    await expect(page).toHaveScreenshot();
   });
 
   test('After clicking on a quote, the quoted message should be shown at the top of the page and the FAB will be visible. Clicking on the FAB should make the message with the quote show at the top of the page', async () => {
@@ -174,5 +179,141 @@ test.describe('Test the lihkg-snapshot-viewer application using Playwright', () 
 
     await page.locator(Selector.Fab).click();
     expect(await page.locator(Selector.Fab).count()).toEqual(0);
+  });
+});
+
+test.describe('ACT II', () => {
+  let page: Page;
+
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+
+    await page.goto(url);
+
+    const fileChooserPromise = page.waitForEvent('filechooser');
+    await page.locator('input').click();
+    const fileChooser = await fileChooserPromise;
+    await fileChooser.setFiles(
+      path.join(
+        process.env.PWD!,
+        'tests/test-assets/thread_3913245_20250426T155841Z', // This snapshot has downloaded images, and the images at the top are non-animated
+      ),
+    );
+    await page.locator('div[id="138"]').waitFor();
+  });
+
+  test("The title should contain the thread's title", async () => {
+    await expect(page).toHaveTitle(
+      '轉工無人工加但fully remote轉唔轉好 | LIHKG Snapshot Viewer',
+    );
+  });
+
+  test("The thread's title should be shown on the top bar", async () => {
+    expect(await page.locator(Selector.Topbar).innerText()).toStrictEqual(
+      '轉工無人工加但fully remote轉唔轉好',
+    );
+  });
+
+  test('Visual regression testing', async () => {
+    test.skip(!isDarwin);
+    await expect(page).toHaveScreenshot();
+  });
+
+  test('After clicking on a quote, the quoted message should be shown at the top of the page and the FAB will be visible. Clicking on the FAB should make the message with the quote show at the top of the page', async () => {
+    const idOfMessageWithQuote = 134;
+    const idOfQuotedMessage = 100;
+
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"]`)
+      .scrollIntoViewIfNeeded();
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"] > blockquote > a`)
+      .click();
+    await expect(page).toHaveURL(url + `#${idOfQuotedMessage}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(1);
+    await page.locator(Selector.Fab).click();
+    await expect(page).toHaveURL(url + `#${idOfMessageWithQuote}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(0);
+  });
+
+  test('After clicking on a quote, scrolling up and clicking on the FAB, the quoted message should be shown at the top of the page. Clicking on the FAB again should make the message with the quote show at the top of the page', async () => {
+    const idOfMessageWithQuote = 134;
+    const idOfQuotedMessage = 100;
+
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"]`)
+      .scrollIntoViewIfNeeded();
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"] > blockquote > a`)
+      .click();
+    await expect(page).toHaveURL(url + `#${idOfQuotedMessage}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(1);
+    await page.locator('div[id="1"]').scrollIntoViewIfNeeded();
+    await expect(page.locator('div[id="1"]')).toBeInViewport();
+    await page.locator(Selector.Fab).click();
+    await expect(
+      page.locator(`div[id="${idOfQuotedMessage}"]`),
+    ).toBeInViewport();
+    await expect(page).toHaveURL(url + `#${idOfQuotedMessage}`);
+    await page.locator(Selector.Fab).click();
+    await expect(
+      page.locator(`div[id="${idOfMessageWithQuote}"]`),
+    ).toBeInViewport();
+    await expect(page).toHaveURL(url + `#${idOfMessageWithQuote}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(0);
+  });
+
+  test('After clicking on a quote and scrolling down pass the message with the quote, the FAB should disappear', async () => {
+    const idOfMessageWithQuote = 86;
+    const idOfQuotedMessage = 29;
+
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"]`)
+      .scrollIntoViewIfNeeded();
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"] > blockquote > a`)
+      .click();
+    await expect(page).toHaveURL(url + `#${idOfQuotedMessage}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(1);
+    await page
+      .locator(`div[id="${idOfMessageWithQuote + 20}"]`)
+      .scrollIntoViewIfNeeded();
+    await expect(
+      page.locator(`div[id="${idOfMessageWithQuote + 20}"]`),
+    ).toBeInViewport();
+    expect(await page.locator(Selector.Fab).count()).toEqual(0);
+  });
+
+  test('When clicking on the swap icon button, the file input should be shown. When the swap icon button is clicked again, the file input should be hidden', async () => {
+    await expect(page.locator(Selector.FileInput)).toBeHidden();
+    await expect(page.locator(Selector.TopbarTitleP)).toBeVisible();
+
+    await page.locator(Selector.ChangeInputButton).click();
+
+    await expect(page.locator(Selector.FileInput)).toBeInViewport();
+    await expect(page.locator(Selector.TopbarTitleP)).toBeHidden();
+
+    await page.locator(Selector.ChangeInputButton).click();
+
+    await expect(page.locator(Selector.FileInput)).toBeHidden();
+    await expect(page.locator(Selector.TopbarTitleP)).toBeVisible();
+  });
+
+  test('After clicking on a quote quoting the first message, scrolling up to the very top of the page and clicking on the FAB, the message with the quote should be shown at the top of the page', async () => {
+    const idOfMessageWithQuote = 137;
+    const idOfQuotedMessage = 1;
+
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"]`)
+      .scrollIntoViewIfNeeded();
+    await page
+      .locator(`div[id="${idOfMessageWithQuote}"] > blockquote > a`)
+      .click();
+    await expect(page).toHaveURL(url + `#${idOfQuotedMessage}`);
+    expect(await page.locator(Selector.Fab).count()).toEqual(1);
+    await page.locator(Selector.Topbar).scrollIntoViewIfNeeded();
+    await expect(page.locator(Selector.Topbar)).toBeInViewport({ ratio: 1 });
+    await page.locator(Selector.Fab).click();
+    await expect(page).toHaveURL(url + `#${idOfMessageWithQuote}`);
   });
 });
